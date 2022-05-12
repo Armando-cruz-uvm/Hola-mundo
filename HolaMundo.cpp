b9195104ff70fa1d5faeb80f707b8dcddc9a6528
Armando-cruz-uvm <adm_31094@my.uvm.edu.mx>
2022-05-12 07:49:06 -0500

    commit del cliente
    Modifique el readme con mas detalles
    
    agregue el archivo HolaMundo.cpp

--- HolaMundo.cpp
+++ HolaMundo.cpp
@@ -1,11 +1,21 @@
-# Hola-mundo
+/* 
 Autor: Armando Cruz Cruz
 Escuela: UVM Campus Villahermosa
 Materia: Programacion Estructurada
 CICLO: 01/2022
 
-Programa que muestra la estructura b√°sica de un programa en el lenguaje C
+Programa que muestra la estructura b·sica de un programa en el lenguaje C
 Ademas se ve el uso de:
 - Include. para agregar libreriras
 - Comentarios. para la documentacion interna del programa
 - printf. para mostrar datos en pantalla
+
+*/
+// Incluir librerias
+#include<stdio.h>
+// Programa principal
+int main() {
+	printf("Hola Mundo!\n");
+	return 0;
+}
+
